625ffe1204f4e39cb0696f84da3f3df6ae134c98
Jackson Wheeler <jacksonjerrywheeler@gmail.com>
2017-01-19 11:40:56 -0800

    added dynamic resume link

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var http = require("http");
 var url = require("url");
+var request = require("request");
 function start() {
 
 	var express = require('express');
@@ -18,7 +19,23 @@ function start() {
 		res.sendFile(__dirname + '/statics/aboutMe.html')
 	});
 
-	
+	app.get('/resume', function(req,res) {
+		request('http://docs.google.com/document/d/1UfJi7kSP_alS7ct90PUQOBx-cXET3iEQP7ekGsALA8Q/export?format=html', function (error, response, body) {
+		  if (!error && response.statusCode == 200) {
+		    string = body; // Show the HTML for the Google homepage. 
+		    console.log(body)
+		    res.send(body)
+		  }
+		  else {
+		    console.log("Error "+response.statusCode)
+		    string = "error"
+		    res.send("Whoops there was an error. Download my resume here <a href='http://docs.google.com/document/d/1UfJi7kSP_alS7ct90PUQOBx-cXET3iEQP7ekGsALA8Q/export?format=html'>here</a>")
+		  }
+		})
+
+
+		
+	})
 	
 	var port = 8080;
 	if(process.platform == "linux"){
