30425e6541c3b7288ca0c427351c235066fc21f9
Jackson Wheeler <jacksonjerrywheeler@gmail.com>
2018-11-14 11:24:40 -0800

    changed resume loading

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var http = require("http");
 var url = require("url");
 var request = require("request");
+var fs = require('fs');
 function start() {
 
 	var express = require('express');
@@ -21,19 +22,25 @@ function start() {
 	});
 
 	app.get('/resume', function(req,res) {
-		request('http://docs.google.com/document/d/1UfJi7kSP_alS7ct90PUQOBx-cXET3iEQP7ekGsALA8Q/export?format=html', function (error, response, body) {
-		  if (!error && response.statusCode == 200) {
-		    string = body; // Show the HTML for the Google homepage.
-		    var d = new Date() 
-		    console.log("got new resume on " + d )
-		    res.send(body)
-		  }
-		  else {
-		    console.log("Error "+response.statusCode)
-		    string = "error"
-		    res.send("Whoops there was an error. Download my resume here <a href='http://docs.google.com/document/d/1UfJi7kSP_alS7ct90PUQOBx-cXET3iEQP7ekGsALA8Q/export?format=html'>here</a>")
-		  }
-		})
+		//request('http://docs.google.com/document/d/1UfJi7kSP_alS7ct90PUQOBx-cXET3iEQP7ekGsALA8Q/export?format=pdf', function (error, response, body) {
+		  //res.send('<iframe style="width:100%;height:100%;" src="https://docs.google.com/document/d/e/2PACX-1vTREC1lQp039_osbBKFMggbx2jg9BfyppUcSOQUcImKLypc74TexY2W5Opd3IqI3Y8_cKlpiR6y4QcD/pub?embedded=true"></iframe>')
+		  res.sendFile(__dirname + '/statics/resume.html')
+
+		  // if (!error && response.statusCode == 200) {
+		  // 	fs.writeFileSync("statics/resume.pdf", body,'base64');
+		  // 	res.send("<html><script>window.location='/resume.pdf'</script></html>")
+		  // }
+		  // //   string = body; // Show the HTML for the Google homepage.
+		  // //   var d = new Date() 
+		  // //   console.log("got new resume on " + d )
+		  // //   res.send(body)
+		  // // }
+		  // else {
+		  //   console.log("Error "+response.statusCode)
+		  //   string = "error"
+		  //   res.send("Whoops there was an error. Download my resume here <a href='http://docs.google.com/document/d/1UfJi7kSP_alS7ct90PUQOBx-cXET3iEQP7ekGsALA8Q/export?format=html'>here</a>")
+		  // }
+		//})
 
 
 		
